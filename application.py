42b2564da61d96a87a310836a30448b8a18213b1
Bhaskar <Balaji.Bhaskar@elekta.com>
2024-01-31 22:10:26 +0000

    Added missing packages in requirement.txt

--- application.py
+++ application.py
@@ -1,9 +1,6 @@
 from flask import Flask, request, render_template
-import numpy as np
-import pandas as pd
 from src.logger import logging
 from src.pipeline.predict_pipeline import CustomData, PredictPipeline
-from sklearn.preprocessing import StandardScaler
 
 application = Flask(__name__)
 app = application
--- src/utils.py
+++ src/utils.py
@@ -1,7 +1,5 @@
 import os
 import sys
-import pandas as pd
-import numpy as np 
 from src.exception import CustomException
 from src.logger import logging
 import pickle
